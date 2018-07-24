961e89b2595c71403774565bfebb3c59aaa1edd9
Erik <erikwjonsson@gmail.com>
2018-07-24 22:54:39 +0200

    First attempt at styling Card.

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./card.css";
+import "./card.scss";
 
 class Card extends Component {
   constructor(props) {
@@ -8,25 +8,30 @@ class Card extends Component {
 
   render() {
     const { amountUsed, amountLimit, progressBar, endDate } = {
-      amountUsed: 600,
-      amountLimit: 1000,
-      progressBar: 0.3,
-      endDate: "2018-09-01"
+      amountUsed: "600.00 €",
+      amountLimit: "1000.00 €",
+      progressBar: "40",
+      endDate: "By 15th June 2018"
     };
     return (
       <div className="card">
         <div className="image-container">
-          CARD!
           <img
             className="card-image"
             src="src/components/cards/new_york.jpg"
             alt="Smiley face"
           />
+          <p>NEW YORK</p>
         </div>
         <div className="content-container">
-          <div className="amount">{amountUsed}</div>
-          <div className="progress-bar">{progressBar}</div>
-          <div className="amount">{amountLimit}</div>
+          <div className="amount-used">{amountUsed}</div>
+          <div className="progress-bar-container">
+            <div className="progress-bar">
+              <div style={{ width: `${progressBar}%` }} className="progress" />
+            </div>
+            <div className="amount-limit">{amountLimit}</div>
+          </div>
+
           <div className="end-date">{endDate}</div>
         </div>
       </div>
