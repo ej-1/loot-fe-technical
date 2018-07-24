9ff4472c1a7a6c4f5b0b34ab38a018980be42839
Erik <erikwjonsson@gmail.com>
2018-07-25 00:30:58 +0200

    Added details-button to Card. More styling.

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -7,9 +7,10 @@ class Card extends Component {
   }
 
   render() {
-    const { amountUsed, amountLimit, progressBar, endDate } = {
-      amountUsed: "600.00 €",
-      amountLimit: "1000.00 €",
+    const { imageText, amountUsed, amountLimit, progressBar, endDate } = {
+      imageText: "NEW YORK",
+      amountUsed: "£ 600.00",
+      amountLimit: "£ 1000.00",
       progressBar: "40",
       endDate: "By 15th June 2018"
     };
@@ -21,9 +22,11 @@ class Card extends Component {
             src="src/components/cards/new_york.jpg"
             alt="Smiley face"
           />
-          <p>NEW YORK</p>
+          <p>{imageText}</p>
         </div>
+
         <div className="content-container">
+          <div className="details-button"> ...</div>
           <div className="amount-used">{amountUsed}</div>
           <div className="progress-bar-container">
             <div className="progress-bar">
