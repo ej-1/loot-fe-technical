18a5076e10d4dbcd864788d4bfd26e18130e6f4d
Erik <erikwjonsson@gmail.com>
2018-07-30 02:17:02 +0200

    Removed unused styled component Button.

--- frontend/src/components/cards/StyledCard.js
+++ frontend/src/components/cards/StyledCard.js
@@ -140,18 +140,4 @@ const ContentContainerCardDetails = styled.div`
   }
 `
 
-const Button = styled.div`
-  color: #fff;
-  border: 0;
-  border-radius: 2px;
-  background: #00bbc6;
-  width: 90px;
-  font-size: 10px;
-  padding: 5px 15px 5px 15px;
-  text-align: center;
-  background: #54b8c3;
-  display: inline;
-  ${props => props.completed && 'background: #54b8c3;'}
-`
-
-export { StyledCard, CardImageContainer, StyledImg, ContentContainerCard, ProgressBar, AmountLimit, BreadcrumbButton, TextTopLeft, TextBottomCenter, ContentContainerCardDetails, Button }
+export { StyledCard, CardImageContainer, StyledImg, ContentContainerCard, ProgressBar, AmountLimit, BreadcrumbButton, TextTopLeft, TextBottomCenter, ContentContainerCardDetails }
